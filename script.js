41a6fa3bd92811ebf24286b9c73950f9df285aca
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2023-12-22 19:07:22 -0700

    Added input bars as well as functional operations. Added number buttons

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const equalEl = document.getElementById('=').value;
-const submitEl = document.getElementById('submit')
+const submitEl = document.getElementById('submit');
 const firstNum = document.getElementById('input1');
 const secondNum = document.getElementById('input2');
 const subtractEl = document.getElementById('-');
 const addEl = document.getElementById('+');
 const divideEl = document.getElementById('/');
 const multiplyEl = document.getElementById('*');
+const resultEl = document.getElementById('result');
 
 
 
@@ -13,8 +14,8 @@ function arithmetic() {
     function addInputs() {
         const inputValue1 = parseInt(firstNum.value);
         const inputValue2 = parseInt(secondNum.value);
-
-        const sum = inputValue1 + inputValue2
+        const sum = inputValue1 + inputValue2;
+        resultEl.textContent = sum;
         console.log(sum)
 
     }
@@ -22,8 +23,8 @@ function arithmetic() {
     function subtractInputs() {
         const inputValue1 = parseInt(firstNum.value);
         const inputValue2 = parseInt(secondNum.value);
-
-        const subtract = inputValue1 - inputValue2
+        const subtract = inputValue1 - inputValue2;
+        resultEl.textContent = subtract;
         console.log(subtract)
 
     }
@@ -31,8 +32,8 @@ function arithmetic() {
     function divideInputs() {
         const inputValue1 = parseInt(firstNum.value);
         const inputValue2 = parseInt(secondNum.value);
-
-        const divide = inputValue1 / inputValue2
+        const divide = inputValue1 / inputValue2;
+        resultEl.textContent = divide;
         console.log(divide)
 
     }
@@ -40,8 +41,8 @@ function arithmetic() {
     function multiplyInputs() {
         const inputValue1 = parseInt(firstNum.value);
         const inputValue2 = parseInt(secondNum.value);
-
         const multiply = inputValue1 * inputValue2
+        resultEl.textContent = multiply;
         console.log(multiply)
 
     }
@@ -51,17 +52,15 @@ function arithmetic() {
     subtractEl.addEventListener('click', subtractInputs);
     divideEl.addEventListener('click', divideInputs);
     multiplyEl.addEventListener('click', multiplyInputs);
+    
 
 }
-
+console.log(resultEl.innerHTML)
 
 arithmetic();
 
 
 
-// submitEl.addEventListener('click', arithmetic);
 
-// Should i Know try to do this with button clicks?
-// Or should I figure out how to do multiple arithmetics in the same function ?
 
 
